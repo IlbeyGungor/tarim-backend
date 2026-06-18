bd555f236c98744b5d7c54f5b0d82607cdc8eb4f
İlbey Güngör <ilbey.gungor@outlook.com>
2026-06-18 16:29:51 +0200

    API connections 2

--- src/server.js
+++ src/server.js
@@ -62,7 +62,7 @@ app.use(cors({
     callback(null, isAllowedCorsOrigin(origin));
   },
   methods: ['GET','POST','PATCH','DELETE','OPTIONS'],
-  allowedHeaders: ['Content-Type','Authorization'],
+  allowedHeaders: ['Content-Type','Authorization','Cache-Control','Pragma'],
 }));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
